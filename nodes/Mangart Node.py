76e2cca5d6c3b8cc252a82081bc6860113c3f53f
DimaGalkin <dim.galkin@gmail.com>
2021-08-13 22:57:04 -0700

    update

--- nodes/Mangart Node.py
+++ nodes/Mangart Node.py
@@ -1,3 +1,4 @@
+from NAT.NAT import ip_tables
 import socket
 import threading
 import json
@@ -40,6 +41,31 @@ sock.bind(('', 24339))
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.settimeout(1.0)
    
+def rec_ip_tbl():
+    global ip_table
+    data_enc, address = sock.recvfrom(1024)
+    data = data_enc.decode()
+    data = json.loads(data)
+    print(data)
+
+    if(len(data) > len(ip_table)):
+        for i in range(len(ip_table)):
+            if(ip_table[i] != data[i]):
+                ip_table[len(ip_table)] = data[i]
+    elif(len(data) > len(ip_table)):
+        for i in range(len(data)):
+            if(ip_table[i] != data[i]):
+                ip_table[len(ip_table)] = data[i]
+    
+    print(ip_table)
+
+def snd_ip_tbl():
+    global ip_table
+    ip_table_enc = json.dumps(ip_table)
+    for i in range(len(verif)):
+        client_socket.sendto(ip_table_enc.encode(),(str(verif[i]), 24339))
+    time.sleep(2)
+
 def background():
     global ip_table
     while True:
@@ -56,9 +82,8 @@ def background():
             print("Connected with " + dt[0])
         ip_table[dt[1]] = dt[0]
         ip_table_enc = json.dumps(ip_table)
-        print(ip_table)
         server_socket.sendto(ip_table_enc.encode(),(str(dt[0]), 9000))
         time.sleep(2)
 
-b = threading.Thread(name='background', target=background)
-b.start()
+bg = threading.Thread(name='background', target=background)
+bg.start()
